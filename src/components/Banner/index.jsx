7fc30d73818060390f6c00ff731b6dbf71891334
Oliver Liongson <44100716+olsantiago@users.noreply.github.com>
2025-07-08 16:35:32 -0700

    get a free consultation

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -85,7 +85,7 @@ export default function Banner() {
                 <h3>Allies</h3>
                 <div className={styles['btn-box']}>
                   <Button className={styles.button} onClick={() => toggleForm()}>
-                    <span className="txt">Get Free Consultation</span>
+                    <span className="txt">Get Free a Consultation</span>
                   </Button>
                 </div>
               </div>
