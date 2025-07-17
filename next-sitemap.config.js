3eeb420285288acd3244c447127ce2f610188581
Oliver Liongson <44100716+olsantiago@users.noreply.github.com>
2025-07-17 10:26:55 -0700

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: 'https://www.mendezsanchezlaw.com',
+  generateRobotsTxt: true,  // Generate robots.txt,
+  changefreq: 'daily', // Frequency with which the pages are likely to change (daily, weekly, etc.)
+  priority: 0.7,
+};
