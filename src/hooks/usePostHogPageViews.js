77ead0ae68f1e51b7c3fcac3c032df38c3e1fc99
Oliver Liongson <44100716+olsantiago@users.noreply.github.com>
2025-07-17 14:15:14 -0700

    Update usePostHogPageViews.js

--- src/hooks/usePostHogPageViews.js
+++ src/hooks/usePostHogPageViews.js
@@ -4,15 +4,32 @@ import { usePathname, useSearchParams } from 'next/navigation';
 import { usePostHog } from 'posthog-js/react';
 import { useEffect } from 'react';
 
+// Shared environment check function
+const initiateTracking = () => process.env.NODE_ENV !== 'development';
+
 export function usePostHogPageViews() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const posthog = usePostHog();
+  const url = `${pathname}?${searchParams.toString()}`;
 
   useEffect(() => {
-    if (process.env.NODE_ENV !== 'development') {
-      const url = `${pathname}?${searchParams.toString()}`;
+    if (initiateTracking()) {
       posthog.capture('$pageview', { url });
     }
-  }, [pathname, searchParams, posthog]);
+  }, [url, posthog]);
+
+  useEffect(() => {
+    if (initiateTracking()) {
+      const handlePageLeave = () => {
+        posthog.capture('$pageleave', { url });
+      };
+
+      window.addEventListener('beforeunload', handlePageLeave);
+
+      return () => {
+        window.removeEventListener('beforeunload', handlePageLeave);
+      };
+    }
+  }, [url, posthog]);
 }
