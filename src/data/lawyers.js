37344f70d321375421815408c295293109c97446
Oliver Liongson <44100716+olsantiago@users.noreply.github.com>
2025-07-25 09:51:32 -0700

    Update lawyers.js

--- src/data/lawyers.js
+++ src/data/lawyers.js
@@ -9,6 +9,7 @@ import JamesImg from '@images/lawyers/James_Drake.webp';
 import JenniferImg from '@images/lawyers/Jennifer_Russell.webp';
 import MichaelImg from '@images/lawyers/Michael_Sanchez.webp';
 import SamImg from '@images/lawyers/Sam_Hibbs.webp';
+// import RyanImg from '@images/lawyers/Ryan_Larsen.webp';
 
 export const partners = [
   {
@@ -29,39 +30,60 @@ export const partners = [
 
 export const lawyers = [
   {
-    name: 'Alex Guerrero, Esq.',
-    role: 'Partner',
-    description:
-      'With over 17 years of experience as a defense attorney, Alex Guerrero now fights for injury victims. Using insider knowledge of insurance companies and corporate law firms, he maximizes settlements and verdicts for clients with serious personal injury claims.',
-    image: AlexImg.src
-  },
-  {
     name: 'Arya Tahmassebi, Esq.',
-    role: 'Partner',
+    role: 'Managing Partner',
     description:
       'Arya Tahmassebi is a skilled personal injury lawyer representing clients in California and Nevada. He specializes in serious injury, wrongful death, and bad faith insurance claims, and has helped recover millions in compensation through aggressive legal advocacy.',
     image: AryaImg.src
   },
   {
+    name: 'Alex Guerrero, Esq.',
+    role: 'Partner | Trial Attorney',
+    description:
+      'With over 17 years of experience as a defense attorney, Alex Guerrero now fights for injury victims. Using insider knowledge of insurance companies and corporate law firms, he maximizes settlements and verdicts for clients with serious personal injury claims.',
+    image: AlexImg.src
+  },
+  {
     name: 'Jennifer Russell, Esq.',
-    role: 'Senior Attorney',
+    role: 'Partner | Trial Attorney',
     description:
       'Jennifer Russell is a highly skilled personal injury lawyer with extensive experience in complex litigation. Known for her strategic thinking and deep legal knowledge, she is a dedicated advocate who consistently delivers strong results for clients facing serious injury claims.',
     image: JenniferImg.src
   },
   {
     name: 'Arielle Farias, Esq.',
-    role: 'Associate Attorney',
+    role: 'Senior Associate',
     description:
       'Arielle Farias is a dedicated personal injury attorney who passionately represents accident victims. As a proud first-generation Latina lawyer, she offers personalized legal support and helps clients navigate every stage of the claims process to ensure they get the justice they deserve.',
     image: ArielleImg.src
   },
   {
     name: 'Andrew Riccitelli, Esq.',
-    role: 'Associate Attorney',
+    role: 'Senior Associate',
     description:
       'Andrew Riccitelli is a personal injury and wrongful death attorney serving clients across California. As a former defense lawyer, he knows how to navigate complex claims and fight for fair compensation. Andrew has successfully settled and litigated numerous cases, earning a reputation for results-driven legal service.',
     image: AndrewImg.src
+  },
+  {
+    name: 'Sam Hibbs, Esq.',
+    role: 'Senior Associate',
+    description:
+      'Sam Hibbs is a civil trial attorney focusing on personal injury, auto accidents, wrongful death, and product liability. He earned his B.A. from San Diego State University and graduated with distinction from McGeorge School of Law, where he competed nationally in Mock Trial. Sam is committed to understanding each client’s story to build compelling cases.',
+    image: SamImg.src
+  },
+  // {
+  //   name: 'Ryan Larsen, Esq.',
+  //   role: 'Senior Associate',
+  //   description:
+  //     'Ryan Larsen is a seasoned personal injury attorney known for his meticulous preparation and client-first approach. He has handled a wide variety of complex personal injury cases and is passionate about securing justice for accident victims.',
+  //   image: RyanImg.src
+  // },
+  {
+    name: 'Brittany Farias, Esq.',
+    role: 'Associate Attorney',
+    description:
+      'Brittany Farias is a personal injury attorney licensed in California and Nevada. She has handled a wide range of cases, including product liability and sexual misconduct. Brittany holds a Certificate in Dispute Resolution from Pepperdine’s top-ranked Straus Institute and blends strong litigation skills with tactical negotiation.',
+    image: BrittanyImg.src
   },
   {
     name: 'James F. Drake IV, Esq.',
@@ -76,20 +98,5 @@ export const lawyers = [
     description:
       'Harrison Shiels is a passionate trial attorney who graduated from Southwestern Law School in 2023 and holds a degree from the University of Michigan. As co-founder of Foresight Focus Groups, he helps trial lawyers improve their strategies. Outside the courtroom, he’s a DJ, music producer, and fitness enthusiast.',
     image: HarrisonImg.src
-  },
-
-  {
-    name: 'Brittany Farias, Esq.',
-    role: 'Associate Attorney',
-    description:
-      'Brittany Farias is a personal injury attorney licensed in California and Nevada. She has handled a wide range of cases, including product liability and sexual misconduct. Brittany holds a Certificate in Dispute Resolution from Pepperdine’s top-ranked Straus Institute and blends strong litigation skills with tactical negotiation.',
-    image: BrittanyImg.src
-  },
-  {
-    name: 'Sam Hibbs, Esq.',
-    role: 'Associate Attorney',
-    description:
-      'Sam Hibbs is a civil trial attorney focusing on personal injury, auto accidents, wrongful death, and product liability. He earned his B.A. from San Diego State University and graduated with distinction from McGeorge School of Law, where he competed nationally in Mock Trial. Sam is committed to understanding each client’s story to build compelling cases.',
-    image: SamImg.src
   }
 ];
