ba3af7d916d8faa7610bafaae44498f0bc3a3962
Oliver Liongson <44100716+olsantiago@users.noreply.github.com>
2025-08-05 14:49:03 -0700

    Update useUtmData.js

--- src/hooks/useUtmData.js
+++ src/hooks/useUtmData.js
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 
 export function useUtmData() {
@@ -9,18 +10,23 @@ export function useUtmData() {
     utm_content: null
   });
 
+  const router = useRouter();
+
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const params = new URLSearchParams(window?.location?.search);
+      const isLandingPage =
+        router.pathname.startsWith('/landing') || router.pathname.startsWith('/es/landing');
+
+      const params = new URLSearchParams(window.location.search);
       setUtmData({
-        utm_campaign: params?.get('utm_campaign'),
-        utm_source: params?.get('utm_source'),
-        utm_medium: params?.get('utm_medium'),
-        utm_term: params?.get('utm_term'),
-        utm_content: params?.get('utm_content')
+        utm_campaign: params.get('utm_campaign') || params.get('gad_campaignid'),
+        utm_source: isLandingPage ? 'landingpage' : params.get('utm_source'),
+        utm_medium: params.get('utm_medium'),
+        utm_term: params.get('utm_term'),
+        utm_content: params.get('utm_content')
       });
     }
-  }, []);
+  }, [router.pathname]);
 
   return utmData;
 }
