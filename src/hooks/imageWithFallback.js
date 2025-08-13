10363aca0e0a4ff31fc56f8f4430b11d8884c393
Oliver Liongson <44100716+olsantiago@users.noreply.github.com>
2025-08-13 16:28:18 -0700

    Update imageWithFallback.js

--- src/hooks/imageWithFallback.js
+++ src/hooks/imageWithFallback.js
@@ -1,10 +1,16 @@
 'use client';
 
+import PropTypes from 'prop-types';
 import fallbackImage from '@images/intro/ms-banner.webp';
 import Image from 'next/image';
 import { useState } from 'react';
 
-export default function ImageWithFallback({ src, fallbackSrc = fallbackImage.src, alt, ...props }) {
+export default function ImageWithFallback({
+  src,
+  fallbackSrc = fallbackImage.src,
+  alt,
+  ...props
+}) {
   const [imgSrc, setImgSrc] = useState(src);
 
   return (
@@ -20,3 +26,9 @@ export default function ImageWithFallback({ src, fallbackSrc = fallbackImage.src
     />
   );
 }
+
+ImageWithFallback.propTypes = {
+  src: PropTypes.string.isRequired,
+  fallbackSrc: PropTypes.string,
+  alt: PropTypes.string.isRequired,
+};
